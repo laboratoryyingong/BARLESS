59fb64f537c5a456e8d1e937a770806e8b6b70b8
YIN GONG <max.g.laboratory@gmail.com>
2016-01-11 17:06:52 +1030

    minor changes

--- www/public/javascript/controllers.js
+++ www/public/javascript/controllers.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('todoApp',[]);
+var myApp = angular.module('todoApp', ['ui.sortable']);
 
 myApp.filter('greet', function(){
     return function(name){
@@ -16,7 +16,7 @@ myApp.directive('customModals', function( $http, $compile){
         },
         template: '',
         link: function(scope, el, attrs){
-            console.log(scope);
+//            console.log(scope);
             scope.modalId = attrs.modalId;
             scope.title = attrs.modalTitle;
 
@@ -32,3 +32,75 @@ myApp.directive('customModals', function( $http, $compile){
 
 });
 
+
+
+myApp.controller('sortableController', function($scope) {
+
+  var tmpList = [];
+
+  for (var i = 1; i <= 3; i++){
+    tmpList.push({
+      text: 'BarCode ' + i,
+      value: i
+    });
+  }
+
+  $scope.list = tmpList;
+
+
+  $scope.sortingLog = [];
+
+  $scope.sortableOptions = {
+    handle: '.myHandle',
+    activate: function() {
+        console.log("activate");
+    },
+    beforeStop: function() {
+        console.log("beforeStop");
+    },
+    change: function() {
+        console.log("change");
+    },
+    create: function() {
+        console.log("create");
+    },
+    deactivate: function() {
+        console.log("deactivate");
+    },
+    out: function() {
+        console.log("out");
+    },
+    over: function() {
+        console.log("over");
+    },
+    receive: function() {
+        console.log("receive");
+    },
+    remove: function() {
+        console.log("remove");
+    },
+    sort: function() {
+        console.log("sort");
+    },
+    start: function() {
+        console.log("start");
+    },
+    update: function(e, ui) {
+      console.log("update");
+
+      var logEntry = tmpList.map(function(i){
+        return i.value;
+      }).join(', ');
+      $scope.sortingLog.push('Update: ' + logEntry);
+    },
+    stop: function(e, ui) {
+      console.log("stop");
+
+      // this callback has the changed model
+      var logEntry = tmpList.map(function(i){
+        return i.value;
+      }).join(', ');
+      $scope.sortingLog.push('Stop: ' + logEntry);
+    }
+  };
+});
--- www/public/javascript/index.js
+++ www/public/javascript/index.js
@@ -13,50 +13,73 @@ try{
             slideout.toggle();
           });
 
-    }catch(e){
-        console.log(e.message);
+}catch(e){
+        console.log(e.message);}
+
+
+//try{
+//    var myElement = document.getElementById('slide1');
+//    var myElement2 = document.getElementById('slide2');
+//
+//    var mc = new Hammer(myElement);
+//    var mc2 = new Hammer(myElement2);
+//
+//    mc.on("panleft panright tap press", function(e){
+//        var myElement = document.getElementById('items1');
+//        var myBtn = document.getElementById('delBtn1');
+//
+//        switch(e.type){
+//            case "panleft":
+//                myElement.classList.remove('items-active');
+//                myBtn.classList.remove('delBtn-active');
+//                break;
+//            case "panright":
+//                myElement.classList.add('items-active');
+//                myBtn.classList.add('delBtn-active');
+//                break;
+//        }
+//    });
+//
+//    mc2.on("panleft panright tap press", function(e){
+//        var myElement2 = document.getElementById('items2');
+//        var myBtn2 = document.getElementById('delBtn2');
+//
+//        switch(e.type){
+//            case "panleft":
+//                myElement2.classList.remove('items-active');
+//                myBtn2.classList.remove('delBtn-active');
+//                break;
+//            case "panright":
+//                myElement2.classList.add('items-active');
+//                myBtn2.classList.add('delBtn-active');
+//                break;
+//        }
+//    });
+//
+//   }catch(e){
+//       console.log(e.message);
+//   }
+$("#test1").click(function(){
+    alert("Please press this one, Thanks")
+});
+
+var myElement = document.getElementsByClassName('item');
+
+var mc = new Hammer(myElement);
+
+mc.on("panleft panright tap press", function(e){
+    switch(e.type){
+        case "panleft":
+            console.log(e.type);
+            break;
+
+        case "panright":
+            console.log(e.type);
+            break;
+
+        case "press":
+            alert("HAHA");
+            break;
+
     }
-
-
-try{
-    var myElement = document.getElementById('slide1');
-    var myElement2 = document.getElementById('slide2');
-
-    var mc = new Hammer(myElement);
-    var mc2 = new Hammer(myElement2);
-
-    mc.on("panleft panright tap press", function(e){
-        var myElement = document.getElementById('items1');
-        var myBtn = document.getElementById('delBtn1');
-
-        switch(e.type){
-            case "panleft":
-                myElement.classList.remove('items-active');
-                myBtn.classList.remove('delBtn-active');
-                break;
-            case "panright":
-                myElement.classList.add('items-active');
-                myBtn.classList.add('delBtn-active');
-                break;
-        }
-    });
-
-    mc2.on("panleft panright tap press", function(e){
-        var myElement2 = document.getElementById('items2');
-        var myBtn2 = document.getElementById('delBtn2');
-
-        switch(e.type){
-            case "panleft":
-                myElement2.classList.remove('items-active');
-                myBtn2.classList.remove('delBtn-active');
-                break;
-            case "panright":
-                myElement2.classList.add('items-active');
-                myBtn2.classList.add('delBtn-active');
-                break;
-        }
-    });
-
-   }catch(e){
-       console.log(e.message);
-   }
+});
